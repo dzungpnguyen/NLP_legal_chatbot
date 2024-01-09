f5d153c2c10fc62f92e51015e53bcf3eaee6d737
Adam Louafi <61043077+adamlouafi@users.noreply.github.com>
2024-01-09 11:07:42 +0100

    add chat route

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from ml_based_chatbot.MLChatBot import get_response
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/chat', methods=['GET'])
+def chat():
+    input = request.get_json()["text"]
+    if input is None:
+        return jsonify({"No text in request"}), 400
+    response = get_response(input_text)
+    return jsonify({'response': response}), 200
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
